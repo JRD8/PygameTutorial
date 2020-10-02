1ef71ccb6186089034885bc06482e1017c9dfd73
JRD8 <jdforbew@gmail.com>
2020-10-01 23:38:15 -0400

    Finished Tutorial 4, Including Refactoring Main.py for OOP

--- main.py
+++ main.py
@@ -19,101 +19,96 @@ char = pygame.image.load('assets/standing.png')
 clock = pygame.time.Clock()
 
 
+class player(object):
+	def __init__(self, x, y, width, height):
+		self.x = x
+		self.y = y
+		self.width = width
+		self.height = height
+		self.vel = 10 # Velocity, set how fast the character moves
+		self.isJump = False # Jump action flag
+		self.jumpCount = 10 # Jump action counter
+		self.left = False # Left walk action flag
+		self.right = False # Right walk action flag
+		self.walkCount = 0 # Walk action counter
+		return
+
+	def draw(self, win):
+		if self.walkCount + 1 >= 27:
+			self.walkCount = 0
+
+		if self.left: # If we are walking left, ie left = True
+			win.blit(walkLeft[self.walkCount//3], (self.x, self.y)) # Index to the correct frame, using integer division
+			self.walkCount += 1
+		elif self.right: # If we are walking right, ie right = True
+			win.blit(walkRight[self.walkCount//3], (self.x, self.y)) # Index to the correct frame, using integer division
+			self.walkCount += 1
+		else: # If we are standing still or jumping
+			win.blit(char, (self.x, self.y)) # In that case, show the standing character
+		return
+
+
+
 # Attributes for the initial rectangle character
 x = 50
 y = 400	
 width = 64 # Use the width dimension of the sprite
 height = 64 # Use the height dimension of the sprite
-vel = 10 # Velocity, set how fast the character moves
-isJump = False # Jump action flag
-jumpCount = 10 # Jump action counter
-left = False # Left walk action flag
-right = False # Right walk action flag
-walkCount = 0 # Walk action counter
 
 
 def redrawGameWindow():
-	#win.fill((0,0,0)) # Fill the screen all black to refresh & not have residual char lines
-	global walkCount
 	win.blit(bg, (0,0))
-
-	# pygame.draw.rect(win, (255,0,0), (x, y, width, height)) # Define/redefine red obj to be drawn
-	
-	if walkCount + 1 >= 27:
-		walkCount = 0
-
-	# Draw sprites routine
-	if left: # If we are walking left, ie left = True
-		win.blit(walkLeft[walkCount//3], (x,y)) # Index to the correct frame, using integer division
-		walkCount += 1
-	elif right: # If we are walking right, ie right = True
-		win.blit(walkRight[walkCount//3], (x,y)) # Index to the correct frame, using integer division
-		walkCount += 1
-	else: # If we are standing still or jumping
-		win.blit(char, (x,y)) # In that case, show the standing character
-
-
-	pygame.display.update() # Have to update the display to draw the object 
+	man.draw(win)
+	pygame.display.update() # Have to update the display to draw the object
+	return
 
 # Main loop
+man = player(50, 400, 64, 64) # Instantiate the player with default/init values
 run = True
+
 while run:
-	#pygame.time.delay(50) # A small timedelay in milliseconds.  So, doesn't run too fast
+
 	clock.tick(27) # Set FPS
 
-	# TODO: RESEARCH PYGAME EVENTS
 	for event in pygame.event.get(): # Check pygame for any events/anything that happens from the user
 		if event.type == pygame.QUIT: # If quit event, quit the loop
 			run = False
 
 	keys = pygame.key.get_pressed()
 
-	# TODO: RESEARCH PYGAME KEYS
-	if keys[pygame.K_LEFT] and x > 0: # Have to constrain the edges...
-		x -= vel
-		left = True # Toggle the directional flags
-		right = False # Toggle the directional flags
+	if keys[pygame.K_LEFT] and man.x > 0: # Have to constrain the edges...
+		man.x -= man.vel
+		man.left = True # Toggle the directional flags
+		man.right = False # Toggle the directional flags
 	
-	elif keys[pygame.K_RIGHT] and x < (WIN_WIDTH - width): # Have to constrain the edges...
-		x += vel
-		left = False # Toggle the directional flags
-		right = True # Toggle the directional flags
+	elif keys[pygame.K_RIGHT] and man.x < (WIN_WIDTH - man.width): # Have to constrain the edges...
+		man.x += man.vel
+		man.left = False # Toggle the directional flags
+		man.right = True # Toggle the directional flags
 	
 	else:
-		left = False
-		right = False
-		walkCount = 0
+		man.left = False
+		man.right = False
+		man.walkCount = 0
 	
 
-	if not (isJump): # Constrain up/down/additional jumps during jumps
-		# Up/Down functionality removed in Tutorial #3 in order to animate cg
-			# if keys[pygame.K_UP] and y > 0: # Have to constrain the edges...
-			# 	y -= vel
-			# if keys[pygame.K_DOWN] and y < (WIN_HEIGHT - height): # Have to constrain the edges...
-			# 	y += vel
+	if not (man.isJump): # Constrain up/down/additional jumps during jumps
 		if keys[pygame.K_SPACE]: # Jump key
-			isJump = True # Toggle jump action flag
-			left = False
-			right = False
-			walkCount = 0
+			man.isJump = True # Toggle jump action flag
+			man.left = False
+			man.right = False
+			man.walkCount = 0
 	else:
 		neg = 1
-		if jumpCount >= -10:
-			if jumpCount < 0:
+		if man.jumpCount >= -10:
+			if man.jumpCount < 0:
 				neg = -1
-			y -= (jumpCount ** 2) * 0.5 * neg
-			jumpCount -= 1
+			man.y -= (man.jumpCount ** 2) * 0.5 * neg
+			man.jumpCount -= 1
 		else:
-			isJump = False # Reset jump action flag
-			jumpCount = 10 # Reinitialize the jump counter
+			man.isJump = False # Reset jump action flag
+			man.jumpCount = 10 # Reinitialize the jump counter
 
-
-
-	# TODO: RESEARCH PYGAME SHAPES IN LIBRARY
-	# Code has been moved outside of main loop and refactored into a def redrawGameWindow() function
-		# win.fill((0,0,0)) # Fill the screen all black to refresh & not have residual char lines
-		# pygame.draw.rect(win, (255,0,0), (x, y, width, height)) # Define/redefine red obj to be drawn
-		# pygame.display.update() # Have to update the display to draw the object 
 	redrawGameWindow()
 
 pygame.quit()
