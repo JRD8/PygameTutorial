a7d0b5bbd4d35f95f617e16c378f073ddf144cd0
JRD8 <jdforbew@gmail.com>
2020-10-03 20:22:35 -0400

    Finished Tutorial 6

--- main.py
+++ main.py
@@ -16,6 +16,48 @@ char = pygame.image.load('assets/standing.png')
 
 clock = pygame.time.Clock()
 
+class enemy(object):
+	walkRight = [pygame.image.load("assets/R1E.png"), pygame.image.load('assets/R2E.png'), pygame.image.load('assets/R3E.png'), pygame.image.load('assets/R4E.png'), pygame.image.load('assets/R5E.png'), pygame.image.load('assets/R6E.png'), pygame.image.load('assets/R7E.png'), pygame.image.load('assets/R8E.png'), pygame.image.load('assets/R9E.png'), pygame.image.load('assets/R10E.png'), pygame.image.load('assets/R11E.png')]
+	walkLeft = [pygame.image.load('assets/L1E.png'), pygame.image.load('assets/L2E.png'), pygame.image.load('assets/L3E.png'), pygame.image.load('assets/L4E.png'), pygame.image.load('assets/L5E.png'), pygame.image.load('assets/L6E.png'), pygame.image.load('assets/L7E.png'), pygame.image.load('assets/L8E.png'), pygame.image.load('assets/L9E.png'), pygame.image.load('assets/L10E.png'), pygame.image.load('assets/L11E.png')]
+
+	def __init__(self, x, y, width, height, end):
+		self.x = x
+		self.y = y
+		self.width = width
+		self.height = height
+		self.end = end
+		self.walkCount = 0 # Walk action counter
+		self.vel = 3
+		self.path = [self.x, self.end] # X Coordinates of where enemy starts and where ends
+
+	def draw(self, win):
+		self.move()
+
+		if self.walkCount + 1 >= 33: # 33 = 3 x 11 enemy sprite frames
+			self.walkCount = 0
+
+		if self.vel > 0: # If vel is positive, enemy is moving right
+			win.blit(self.walkRight[self.walkCount//3], (self.x, self.y)) # Index to the correct frame, using integer division
+			self.walkCount += 1
+		else: # If vel is negative, enemy is moving left
+			win.blit(self.walkLeft[self.walkCount//3], (self.x, self.y)) # Index to the correct frame, using integer division
+			self.walkCount += 1
+
+
+	def move(self):
+		if self.vel > 0: # If vel is positive, enemy is moving right
+			if self.x + self.vel < self.path[1]: # If before the end of path
+				self.x += self.vel
+			else: # At end of path, flip and switch direction
+				self.vel = self.vel * -1
+				self.walkCount = 0
+		else: # If vel is negative, enemy is moving left
+			if self.x - self.vel > self.path[0]: # If before the beginning of path
+				self.x += self.vel
+			else: # At end of path, flip and switch direction
+				self.vel = self.vel * -1
+				self.walkCount = 0
+
 
 class projectile(object):
 	def __init__(self, x, y, radius, color, facing):
@@ -43,10 +85,9 @@ class player(object):
 		self.right = False # Right walk action flag
 		self.walkCount = 0 # Walk action counter
 		self.standing = True # Standing or moving flag
-		return
 
 	def draw(self, win):
-		if self.walkCount + 1 >= 27:
+		if self.walkCount + 1 >= 27: # 27 = 3 x 9 player sprite frames
 			self.walkCount = 0
 
 		if not (self.standing): # If moving...
@@ -62,19 +103,19 @@ class player(object):
 				win.blit(walkRight[0], (self.x, self.y))
 			else:
 				win.blit(walkLeft[0], (self.x, self.y))
-		return
 
 
 def redrawGameWindow():
 	win.blit(bg, (0,0))
 	man.draw(win) # Draw the player
+	goblin.draw(win) # Draw the enemy
 	for bullet in bullets: # Draw the bullets
 		bullet.draw(win)
 	pygame.display.update() # Have to update the display to draw the object
-	return
 
 # Main loop
-man = player(50, 400, 64, 64) # Instantiate the player with default/init values, 64 x 64 = dimensions of the sprite
+man = player(250, 400, 64, 64) # Instantiate the player with default/init values, 64 x 64 = dimensions of the sprite
+goblin = enemy(30, 400, 64, 64, 400) # Instantiate the enemy with default/init values, 64 x 64 = dimensions of the sprite
 bullets = [] # This list is way to implement multiple bullets.
 run = True
 
