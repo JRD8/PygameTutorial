7238ca5f517b5fb41e4d0a70d571824b3d012a74
JRD8 <jdforbew@gmail.com>
2020-10-04 21:19:03 -0400

    Slight Tweak To Goblin Hit Score Indicator

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ clock = pygame.time.Clock()
 class enemy(object):
 	walkRight = [pygame.image.load("assets/R1E.png"), pygame.image.load('assets/R2E.png'), pygame.image.load('assets/R3E.png'), pygame.image.load('assets/R4E.png'), pygame.image.load('assets/R5E.png'), pygame.image.load('assets/R6E.png'), pygame.image.load('assets/R7E.png'), pygame.image.load('assets/R8E.png'), pygame.image.load('assets/R9E.png'), pygame.image.load('assets/R10E.png'), pygame.image.load('assets/R11E.png')]
 	walkLeft = [pygame.image.load('assets/L1E.png'), pygame.image.load('assets/L2E.png'), pygame.image.load('assets/L3E.png'), pygame.image.load('assets/L4E.png'), pygame.image.load('assets/L5E.png'), pygame.image.load('assets/L6E.png'), pygame.image.load('assets/L7E.png'), pygame.image.load('assets/L8E.png'), pygame.image.load('assets/L9E.png'), pygame.image.load('assets/L10E.png'), pygame.image.load('assets/L11E.png')]
+	hits_font = pygame.font.SysFont("comicsans", 30)
 
 	def __init__(self, x, y, width, height, end):
 		self.x = x
@@ -47,8 +48,7 @@ class enemy(object):
 			self.walkCount += 1
 		self.hitbox = (self.x + 17, self.y + 2, 31, 57)
 
-		hits_font = pygame.font.SysFont("comicsans", 30)
-		hits_label = hits_font.render(f"Goblin Hits: {self.numHits}", 1, (255,0,0))
+		hits_label = self.hits_font.render(f"Goblin Hits: {self.numHits}", 1, (255,0,0))
 		win.blit(hits_label, (WIN_WIDTH/2 - hits_label.get_width()/2, 10))
 		
 		pygame.draw.rect(win, (255,0,0), (self.hitbox), 2)
